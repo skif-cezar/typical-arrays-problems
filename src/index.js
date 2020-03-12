14c62bacd11aecb15bc31dc208935ebbdb059ec2
skif-cezar <skif.cezar@gmail.com>
2020-03-12 14:37:34 +0300

    fixed solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 exports.min = function min (array) {
-    if(array == [] || array == undefined){
+    if(!array || array.length == 0){
         return 0;
     }else {
         array.sort((a, b) => a - b);
@@ -9,16 +9,16 @@ exports.min = function min (array) {
 };
 
 exports.max = function max (array) {
-    if(array == [] || array == undefined){
+    if(!array || array.length == 0){
         return 0;
     }else {
         array.sort((a, b) => a - b);
-        console.log(array.pop());
+        return (array.pop());
     }
 };
 
 exports.avg = function avg (array) {
-    if(array == [] || array == undefined){
+    if(!array || array.length == 0){
         return 0;
     }else {
         return (array.reduce((a, b) => (a + b)) / array.length);
